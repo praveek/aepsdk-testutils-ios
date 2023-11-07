109e389c49824b7c97467b61a0b4ea4df7238022
Tim Kim <95260439+timkimadobe@users.noreply.github.com>
2023-11-06 16:02:57 -0800

    New helper method to clear new persistence location with Core 4.2.0 (#2)
    * Adds method to clear new persistence location
    
    * Update access level of FileManager extension
    
    * Rename method to removeAdobeCacheDirectory
    
    Refactor params to allow passing directoryName and appGroup
    Update method docs

--- Sources/FileManager+Testable.swift
+++ Sources/FileManager+Testable.swift
@@ -13,8 +13,8 @@
 @testable import AEPServices
 import Foundation
 
-extension FileManager {
-    public func clearCache() {
+public extension FileManager {
+    func clearCache() {
         let knownCacheItems: [String] = ["com.adobe.edge", "com.adobe.edge.identity", "com.adobe.edge.consent"]
         guard let url = self.urls(for: .cachesDirectory, in: .userDomainMask).first else {
             return
@@ -29,6 +29,41 @@ extension FileManager {
             } catch {
                 print("ERROR DESCRIPTION: \(error)")
             }
+        }
+    }
+    
+    /// Removes the Adobe cache directory within the app's data storage (persistence) from the specified app group's container directory or in the default library directory
+    /// if no app group is provided.
+    ///
+    /// - Parameters:
+    ///   - directoryName: A `String` specifying the name of the directory to remove. Defaults to `"com.adobe.aep.datastore"` if not specified.
+    ///   - appGroup: An optional `String` representing the app group identifier. If provided, the method will look for the directory within the app group container. If `nil`, the method will search in the current application's library directory.
+    /// - Requires: Before calling this method, ensure that the caller has the appropriate permissions to access and modify the file system, especially if working with app group directories.
+    func removeAdobeCacheDirectory(_ directoryName: String = "com.adobe.aep.datastore", with appGroup: String? = nil) {
+        let LOG_TAG = "FileManager"
+        let fileManager = FileManager.default
+
+        // Recreate the directory URL
+        var directoryUrl: URL?
+        if let appGroup = appGroup {
+            directoryUrl = fileManager.containerURL(forSecurityApplicationGroupIdentifier: appGroup)?
+                .appendingPathComponent(directoryName, isDirectory: true)
+        } else {
+            directoryUrl = fileManager.urls(for: .libraryDirectory, in: .allDomainsMask).first?
+                .appendingPathComponent(directoryName, isDirectory: true)
+        }
+
+        guard let directoryUrl = directoryUrl else {
+            Log.error(label: LOG_TAG, "Could not compute the directory URL for removal.")
+            return
+        }
+        
+        // Remove the directory
+        do {
+            try fileManager.removeItem(at: directoryUrl)
+            Log.debug(label: LOG_TAG, "Successfully removed directory at \(directoryUrl.path).")
+        } catch {
+            Log.warning(label: LOG_TAG, "Failed to remove directory at \(directoryUrl.path) with error: \(error)")
         }
     }
 }
